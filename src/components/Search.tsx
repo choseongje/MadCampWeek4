e186f851fadcd8d1157570a5e37e6046bc2e91a8
한종국 <jkookhan03@kaist.ac.kr>
2024-07-19 16:26:36 +0900

    서치 기능 디자인 추가

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
+import styles from "../styles/Search.module.css"; // 스타일 파일 임포트
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -29,7 +30,7 @@ const Search = ({ accessToken, onTrackSelect }: SearchProps) => {
   };
 
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
         type="text"
         placeholder="Search for a song"
@@ -37,9 +38,9 @@ const Search = ({ accessToken, onTrackSelect }: SearchProps) => {
         onChange={(e) => setQuery(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
-      <div>
+      <div className={styles.resultsContainer}>
         {results.map((track) => (
-          <div key={track.id}>
+          <div key={track.id} className={styles.trackItem}>
             <img src={track.album.images[0]?.url} alt={track.name} width="50" />
             <div>{track.name}</div>
             <div>{track.artists[0].name}</div>
