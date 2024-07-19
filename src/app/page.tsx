8b210780918eb5be6f1324378a874ee1b5109596
choseongje <hangoy0606@gmail.com>
2024-07-19 15:04:04 +0900

    web playback SDK 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ const Home = () => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [trackId, setTrackId] = useState<string>("");
   const [deviceId, setDeviceId] = useState<string | null>(null);
+  const [player, setPlayer] = useState<any>(null);
 
   useEffect(() => {
     const token = searchParams.get("access_token");
@@ -31,30 +32,57 @@ const Home = () => {
 
   useEffect(() => {
     if (accessToken) {
-      getDeviceId();
+      const script = document.createElement("script");
+      script.src = "https://sdk.scdn.co/spotify-player.js";
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new Spotify.Player({
+          name: "Web Playback SDK",
+          getOAuthToken: (cb) => {
+            cb(accessToken);
+          },
+          volume: 0.5,
+        });
+
+        player.addListener("ready", ({ device_id }) => {
+          console.log("Ready with Device ID", device_id);
+          setDeviceId(device_id);
+        });
+
+        player.addListener("not_ready", ({ device_id }) => {
+          console.log("Device ID has gone offline", device_id);
+        });
+
+        player.addListener("initialization_error", ({ message }) => {
+          console.error("Failed to initialize", message);
+        });
+
+        player.addListener("authentication_error", ({ message }) => {
+          console.error("Failed to authenticate", message);
+        });
+
+        player.addListener("account_error", ({ message }) => {
+          console.error("Failed to validate Spotify account", message);
+        });
+
+        player.addListener("playback_error", ({ message }) => {
+          console.error("Failed to perform playback", message);
+        });
+
+        player.connect();
+        setPlayer(player);
+      };
     }
   }, [accessToken]);
-
-  const getDeviceId = () => {
-    spotifyApi.getMyDevices().then(
-      (data) => {
-        const devices = data.devices;
-        if (devices.length > 0) {
-          setDeviceId(devices[0].id);
-        } else {
-          console.error("No devices found");
-        }
-      },
-      (err) => {
-        console.error("Error fetching devices", err);
-      }
-    );
-  };
 
   const handleLogin = () => {
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
     const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
-    const scopes = "user-read-playback-state user-modify-playback-state";
+    const scopes =
+      "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state";
     window.location.href = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(
       scopes
     )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
@@ -62,18 +90,20 @@ const Home = () => {
 
   const handlePlay = () => {
     if (accessToken && trackId && deviceId) {
-      spotifyApi
-        .play({ device_id: deviceId, uris: [`spotify:track:${trackId}`] })
-        .then(
-          () => {
-            console.log("Track is playing");
-          },
-          (err) => {
-            console.error("Error playing track", err);
-          }
-        );
+      spotifyApi.transferMyPlayback([deviceId]).then(() => {
+        spotifyApi
+          .play({ device_id: deviceId, uris: [`spotify:track:${trackId}`] })
+          .then(
+            () => {
+              console.log("Track is playing");
+            },
+            (err) => {
+              console.error("Error playing track", err);
+            }
+          );
+      });
     } else {
-      console.error("Missing access token, track ID, or device ID");
+      console.error("Missing access token, track ID, or player");
     }
   };
 
