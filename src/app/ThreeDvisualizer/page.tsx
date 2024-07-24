8d28ae845aba10cc35104024113c5032b1df28e3
choseongje <hangoy0606@gmail.com>
2024-07-24 17:23:09 +0900

    3D 비주얼라이저 페이지 로고 추가

--- src/app/ThreeDvisualizer/page.tsx
+++ src/app/ThreeDvisualizer/page.tsx
@@ -1,12 +1,29 @@
-'use client';
+"use client";
 
-import React from 'react';
-import ThreeDVisualizer from '../../components/ThreeDVisualizer';
-import styles from '../../styles/ThreeDVisualizerPage.module.css';
+import React from "react";
+import { useRouter } from "next/navigation";
+import ThreeDVisualizer from "../../components/ThreeDVisualizer";
+import styles from "../../styles/ThreeDVisualizerPage.module.css";
+import Image from "next/image";
+import Logo from "../../../public/YaOng4.png";
 
 const ThreeDVisualizerPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
   return (
     <div className={styles.container}>
+      <Image
+        src={Logo}
+        alt="Logo"
+        className={styles.logo}
+        onClick={handleLogoClick}
+        width={80} // 원하는 width 값 설정
+        height={80} // 원하는 height 값 설정
+      />
       <h1 className={styles.header}>3D Visualizer</h1>
       <ThreeDVisualizer />
     </div>
